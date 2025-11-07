0b1db10df95e89f55a789b285f3553573833600a
Omer Elovici <omerelov1@gmail.com>
2025-11-07 20:49:25 +0200

    fix: normalize std qualifiers and uint aliases.

--- include/endianness.hpp
+++ include/endianness.hpp
@@ -10,7 +10,7 @@ namespace RtpCpp {
     #include <stdlib.h> // for _byteswap_* functions
 #endif
 
-static constexpr std::uint16_t swap_ushort(std::uint16_t bytes) {
+static constexpr uint16_t swap_ushort(uint16_t bytes) {
 #if defined(__clang__) || defined(__GNUC__)
     return __builtin_bswap16(bytes);
 #elif defined(_MSC_VER)
@@ -22,7 +22,7 @@ static constexpr std::uint16_t swap_ushort(std::uint16_t bytes) {
 #endif
 }
 
-static constexpr std::uint32_t swap_ulong(std::uint32_t bytes) {
+static constexpr uint32_t swap_ulong(uint32_t bytes) {
 #if defined(__clang__) || defined(__GNUC__)
     return __builtin_bswap32(bytes);
 #elif defined(_MSC_VER)
@@ -35,7 +35,7 @@ static constexpr std::uint32_t swap_ulong(std::uint32_t bytes) {
 #endif
 }
 
-static constexpr std::uint64_t swap_uint64(std::uint64_t bytes) {
+static constexpr uint64_t swap_uint64(uint64_t bytes) {
 #if defined(__clang__) || defined(__GNUC__)
     return __builtin_bswap64(bytes);
 #elif defined(_MSC_VER)
@@ -54,31 +54,31 @@ static constexpr std::uint64_t swap_uint64(std::uint64_t bytes) {
 }
 
 template <typename T>
-concept UintType = std::same_as<T, std::uint16_t> || std::same_as<T, std::uint32_t> ||
-                   std::same_as<T, std::uint64_t>;
+concept UintType = ::std::same_as<T, uint16_t> || ::std::same_as<T, uint32_t> ||
+                   ::std::same_as<T, uint64_t>;
 
 
-template <std::same_as<std::uint8_t> T>
-void write_big_endian(std::uint8_t* buffer, T data) {
+template <::std::same_as<uint8_t> T>
+void write_big_endian(uint8_t* buffer, T data) {
     *buffer = data;
 }
 
-template <std::same_as<std::uint16_t> T>
-void write_big_endian(std::uint8_t* buffer, const T data) {
-    buffer[0] = static_cast<std::uint8_t>((data & 0xFF'00U) >> 8);
-    buffer[1] = static_cast<std::uint8_t>(data & 0x00'FFU);
+template <::std::same_as<uint16_t> T>
+void write_big_endian(uint8_t* buffer, const T data) {
+    buffer[0] = static_cast<uint8_t>((data & 0xFF'00U) >> 8);
+    buffer[1] = static_cast<uint8_t>(data & 0x00'FFU);
 }
 
-template <std::same_as<std::uint32_t> T>
-void write_big_endian(std::uint8_t* buffer, const T data) {
+template <::std::same_as<uint32_t> T>
+void write_big_endian(::std::uint8_t* buffer, const T data) {
     buffer[0] = (data & 0xFF'00'00'00U) >> 24;
     buffer[1] = (data & 0x00'FF'00'00U) >> 16;
     buffer[2] = (data & 0x00'00'FF'00U) >> 8;
     buffer[3] = (data & 0x00'00'00'FFU);
 }
 
-template <std::same_as<std::uint64_t> T>
-void write_big_endian(std::uint8_t* buffer, const T data) {
+template <::std::same_as<uint64_t> T>
+void write_big_endian(uint8_t* buffer, const T data) {
     buffer[0] = (data & 0xFF'00'00'00'00'00'00'00ULL) >> 56;
     buffer[1] = (data & 0x00'FF'00'00'00'00'00'00ULL) >> 48;
     buffer[2] = (data & 0x00'00'FF'00'00'00'00'00ULL) >> 40;
@@ -90,12 +90,12 @@ void write_big_endian(std::uint8_t* buffer, const T data) {
 }
 
 template <UintType T>
-T read_big_endian(const std::uint8_t* buffer) {
-    if constexpr (std::same_as<T, std::uint16_t>) {
+T read_big_endian(const uint8_t* buffer) {
+    if constexpr (::std::same_as<T, uint16_t>) {
         return (buffer[0] << 8U) | buffer[1];
     }
 
-    if constexpr (std::same_as<T, std::uint32_t>) {
+    if constexpr (::std::same_as<T, uint32_t>) {
         return ((buffer[0] << 24U) | (buffer[1] << 16U) | (buffer[2] << 8U) | buffer[3]);
     }
 }
